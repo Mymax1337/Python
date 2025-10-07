f92d55ab8479273fb5a7981017eeb4290afb3493
Mymax1337 <ramonnyer@gmail.com>
2025-10-07 00:27:12 -0300

    Dunder e condicionais

--- hello.py
+++ hello.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 
-print ("hello, world!")
+"""Hello WOrld multi linguas.
 
-#Este programa imprime helloworld
+Dependendo da lingua configurada no ambiente
+o pograma exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+Expor LANG=pt_BR
+
+Execucao:
+
+Python3 hello.py
+
+ou
+
+./hello.py
+"""
+#Dunder significa 2 underlines no comeco e no fim
+__version__ = "3.12.3"
+__author__ = "Mymax1337"
+__license__ ="Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "pt_BR")[:5]
+msg = "Hello, Wold!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao,Mundo!"
+
+    print(msg)
